f4eea07264252e099d4eba8b64d6aae174f92f80
Anthony Song <songzixing98@gmail.com>
2018-07-06 14:48:57 +0800

    add 785 bfs

--- 785. Is Graph Bipartite?/Solution.cpp
+++ 785. Is Graph Bipartite?/Solution.cpp
@@ -1,3 +1,4 @@
+//dfs
 class Solution {
 public:
     void dfs(vector<vector<int>>& graph,int v,vector<int> &visited,int b){
@@ -39,5 +40,56 @@ public:
         if(flag==1) return true;
         else return false;
       
+    }
+};
+
+
+
+//bfs
+class Solution {
+public:
+    void bfs(vector<vector<int>>& graph,vector<int>& visited,int s){
+        queue<int> Q;
+        Q.push(s);
+    
+        int b=1;
+        visited[s]=b;
+        while(!Q.empty())
+        {
+            int v=Q.front();
+            Q.pop();
+            b=visited[v];
+            b=b%2+1;
+            for(int i=0;i<graph[v].size();i++)
+            {
+                if(visited[graph[v][i]]==0)
+                {visited[graph[v][i]]=b;    
+                    Q.push(graph[v][i]);
+                }
+            }
+        }
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        vector<int> visited(graph.size(),0);
+        if(graph.size()==0) return false;
+        
+        for(int p=0;p<graph.size();p++)
+        {
+            if(visited[p]==0) 
+            bfs(graph,visited,p);
+        }
+        
+        int flag=1;
+        for(int j=0;j<graph.size();j++)
+        {
+            for(int k=0;k<graph[j].size();k++)
+               // if()
+                if(visited[graph[j][k]]==visited[j]) flag=0;
+        }
+      
+        
+        if(flag==1) return true;
+        else return false;
     }
 };
